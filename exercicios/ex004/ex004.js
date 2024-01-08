d0b2dc9fde1929ef34b9d75d8fc588cfce9617b6
BeHaupt <bersurf@outlook.com>
2024-01-08 10:09:39 -0300

    Js studies

--- exercicios/ex004/ex004.js
+++ exercicios/ex004/ex004.js
@@ -1,5 +1,5 @@
 const colors = require ('colors')
-
+//Formatação representar dados em forma de dinheiro//
 var n1 =2080.5
 console.log('Imprimindo variaveis numericas representadas em dinheiro.' .bgGreen)
 console.log(n1.toLocaleString('pr-Br', {style: 'currency', currency: 'EUR'}))
